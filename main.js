126f7963c919ab30fb59c97eb83bdea1f201f170
Devpph <thomasanthony999@gmail.com>
2022-07-27 20:44:59 +0630

    vue app

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: 0,
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://vue-socks.timirwing.dev/img/greenSocks.fcf06f2c.jpg',
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        decrementCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1;
+            }
+        }
+    }
+})
